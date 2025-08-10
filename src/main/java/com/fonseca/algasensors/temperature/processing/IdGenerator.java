543989c90d9f3f1f6af1b59d9edf74f2d1f397ce
samueljff <samueljosef3@gmail.com>
2025-08-10 01:12:16 -0300

    Fix ajustes nas pastas

---
+++ src/main/java/com/fonseca/algasensors/temperature/processing/IdGenerator.java
@@ -0,0 +1,17 @@
+package com.fonseca.algasensors.temperature.processing;
+
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.TimeBasedEpochGenerator;
+
+import java.util.UUID;
+
+public class IdGenerator {
+  private static final TimeBasedEpochGenerator timeBasedEpochGenerator = Generators.timeBasedEpochGenerator();
+
+  private IdGenerator(){
+  }
+
+  public static UUID generateTimeBasedUUID(){
+      return timeBasedEpochGenerator.generate();
+  }
+}
---
+++ src/test/java/com/fonseca/algasensors/temperature/processing/UUIDV7Test.java
@@ -0,0 +1,31 @@
+package com.fonseca.algasensors.temperature.processing;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+public class UUIDV7Test {
+
+    @Test
+    void shouldGeneratedUUIDv7Test (){
+//        UUID uuid1 = IdGenerator.generateTimeBasedUUID();
+//        UUID uuid2 = IdGenerator.generateTimeBasedUUID();
+//        UUID uuid3 = IdGenerator.generateTimeBasedUUID();
+//        UUID uuid4 = IdGenerator.generateTimeBasedUUID();
+//
+//        System.out.println(UUIDv7Utils.extractOffsetDateTime(uuid1));
+//        System.out.println(UUIDv7Utils.extractOffsetDateTime(uuid2));
+//        System.out.println(UUIDv7Utils.extractOffsetDateTime(uuid3));
+//        System.out.println(UUIDv7Utils.extractOffsetDateTime(uuid4));
+
+        UUID uuid = IdGenerator.generateTimeBasedUUID();
+
+        OffsetDateTime offsetDateTime = UUIDv7Utils.extractOffsetDateTime(uuid).truncatedTo(ChronoUnit.MINUTES);
+        OffsetDateTime curentDateTime = OffsetDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+
+        Assertions.assertThat(offsetDateTime).isEqualTo(curentDateTime);
+    }
+}
