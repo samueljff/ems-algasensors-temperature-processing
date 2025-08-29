066caac161669c0308831856ad3dbc7006c2e829
samueljff <samueljosef3@gmail.com>
2025-08-28 23:52:11 -0300

    Publicando mensagens

--- src/main/java/com/fonseca/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
+++ src/main/java/com/fonseca/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -3,7 +3,9 @@ package com.fonseca.algasensors.temperature.processing.api.controller;
 import com.fonseca.algasensors.temperature.processing.api.model.TemperatureLogOutput;
 import com.fonseca.algasensors.temperature.processing.common.IdGenerator;
 import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -11,10 +13,15 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.time.OffsetDateTime;
 
+import static com.fonseca.algasensors.temperature.processing.infrastruture.rabbitmq.RabbitMQConfig.FANOUT_EXCHANGE_NAME;
+
 @RestController
 @RequestMapping("/api/sensors/{sensorId}/temperatures/data")
 @Slf4j
+@RequiredArgsConstructor
 public class TemperatureProcessingController {
+
+    private final RabbitTemplate rabbitTemplate;
 
     @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
     public void data(@PathVariable TSID sensorId, @RequestBody String input) {
@@ -39,6 +46,11 @@ public class TemperatureProcessingController {
 
         log.info(logOutput.toString());
 
+        String exchange = FANOUT_EXCHANGE_NAME;
+        String routingKey = "";
+        Object payload = logOutput;
+
+        rabbitTemplate.convertAndSend(exchange, routingKey, payload);
     }
 
 }
